40c8e5f5f895c7b474d5fc05f3ecd7c901df7651
suyh <su_787910081@sina.com>
2019-11-24 18:40:10 +0800

    log4j2 日志配置文件的详细笔记

--- demo-01-log4j2/src/main/java/Main.java
+++ demo-01-log4j2/src/main/java/Main.java
@@ -2,6 +2,8 @@
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static java.lang.Long.MAX_VALUE;
+
 public class Main {
     static {
         System.setProperty("log4j.configurationFile", "conf/log4j2.xml");
@@ -11,6 +13,15 @@ public class Main {
 
     public static void main(String[] args) {
         logger.info("main starting...");
+        for (long i = 1; i < MAX_VALUE; i++) {
+            logger.info("i: " + i);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                logger.error("InterruptedException", e);
+            }
+        }
+
         logger.info("main finished.");
     }
 }
