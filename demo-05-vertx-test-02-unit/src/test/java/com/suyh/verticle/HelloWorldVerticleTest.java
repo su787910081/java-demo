c8c98d5f2b05bf65a260b49be4daf446f4d39a2d
suyh <su_787910081@sina.com>
2019-11-30 19:39:07 +0800

    vertx 测试用例 - 目前全部都成功运行

--- demo-05-vertx-test-02-unit/src/test/java/com/suyh/verticle/HelloWorldVerticleTest.java
+++ demo-05-vertx-test-02-unit/src/test/java/com/suyh/verticle/HelloWorldVerticleTest.java
@@ -16,11 +16,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.util.concurrent.TimeUnit;
 
+// 官网: https://vertx.io/docs/vertx-web-client/java/
 @ExtendWith(VertxExtension.class)
 public class HelloWorldVerticleTest {
 
     @BeforeEach
-    void deployVerticle(Vertx vertx, VertxTestContext testContext) {
+    void deployVerticle(Vertx vertx, VertxTestContext testContext) throws InterruptedException {
         vertx.deployVerticle(HelloWorldVerticle.class.getName(), testContext.completing());
     }
 
@@ -29,16 +30,34 @@ public class HelloWorldVerticleTest {
     void some_test(Vertx vertx, VertxTestContext testContext) {
         int port = 8000;
 
+        // 部署vertx 服务
+        // testContext.succeeding  部署成功的处理逻辑
         vertx.deployVerticle(new HelloWorldVerticle(), testContext.succeeding(id -> {
+            // 部署成功之后添加测试逻辑
             WebClient client = WebClient.create(vertx);
             client.get(port, "localhost", "/")
                     .as(BodyCodec.string())
                     .send(testContext.succeeding(response -> testContext.verify(() -> {
-//                        Assert.assertThat(response.body()).isEqualTo("Plop");
-                        Assert.assertEquals("", "HelloWorld, I am from port " + port, response.body());
+                        Assert.assertEquals("响应结果出错",
+                                "HelloWorld, I am from port " + port, response.body());
                         testContext.completeNow();
                     })));
         }));
+    }
+
+    @Test
+    void someTest02(Vertx vertx, VertxTestContext testContext) throws InterruptedException {
+        int port = 8000;
+
+        // 部署成功之后添加测试逻辑
+        WebClient client = WebClient.create(vertx);
+        client.get(port, "localhost", "/")
+                .as(BodyCodec.string())
+                .send(testContext.succeeding(response -> testContext.verify(() -> {
+                    Assert.assertEquals("响应结果出错",
+                            "HelloWorld, I am from port " + port, response.body());
+                    testContext.completeNow();
+                })));
     }
 
     @Test
