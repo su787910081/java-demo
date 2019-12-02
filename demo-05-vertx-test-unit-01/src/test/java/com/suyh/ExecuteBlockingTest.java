94238245af39cefe7045b37db31d8940ea6a860c
suyh <su_787910081@sina.com>
2019-12-02 10:59:54 +0800

    在一个测试用例中多次调用

--- demo-05-vertx-test-unit-01/src/test/java/com/suyh/ExecuteBlockingTest.java
+++ demo-05-vertx-test-unit-01/src/test/java/com/suyh/ExecuteBlockingTest.java
@@ -59,4 +59,35 @@ public class ExecuteBlockingTest {
         async.await(5 * 1000);
     }
 
+    // 在一个测试用例中要进行多次异步调用时需要指定调用的个数
+    @Test
+    public void testMany(TestContext ctx) {
+        // 指定要做的调用次数，最多这多次全部都被调用了才结束
+        int count = 2;
+        Async async = ctx.async(count);
+
+        for (int i = 0; i < count; i++) {
+            vertx.executeBlocking(asyncResult -> {
+                int times = timesExecuteBlocking++;
+                if (times % 2 == 0) {
+                    asyncResult.complete(times);
+                } else {
+                    asyncResult.fail("executeBlocking failed.");
+                }
+            }, res -> {
+                if (res.succeeded()) {
+                    int times = (int) res.result();
+                    ctx.assertTrue(times % 2 == 0);
+
+                    async.countDown(); // 指定了个数就不能简单的使用 complete(); 来结束了。
+                } else {
+                    res.cause().printStackTrace();
+                    ctx.fail(res.cause());
+                }
+            });
+        }
+
+        // 前面指定的count 次必须都被正确执行了才结束
+        async.await(5 * 1000);
+    }
 }
