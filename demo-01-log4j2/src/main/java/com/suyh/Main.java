923aafe6b0cb22f0bda8c69350fbc73a6c464461
suyh <su_787910081@sina.com>
2019-12-26 16:16:55 +0800

    pom maven modules: demo-01-log4j2

--- demo-01-log4j2/src/main/java/com/suyh/Main.java
+++ demo-01-log4j2/src/main/java/com/suyh/Main.java
@@ -1,20 +1,20 @@
-import org.junit.Test;
+package com.suyh;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class DemoTest {
+public class Main {
     static {
         System.setProperty("log4j.configurationFile", "conf/log4j2.xml");
         // 对应log4j2.xml 配置文件中使用的 ${sys:log4j2.dir:-demo-log}
-        System.setProperty("log4j2.dir", "demo-test");
+        System.setProperty("log4j2.dir", "demo-01");
     }
 
-    private static final Logger logger = LoggerFactory.getLogger(DemoTest.class);
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
     private static final Logger logSp = LoggerFactory.getLogger("spLogger");
 
-    @Test
-    public void t1() {
-        logger.info("DemoTest starting...");
+    public static void main(String[] args) {
+        logger.info("main starting...");
         for (long i = 1; i < 10; i++) {
             logger.info("i: " + i);
             try {
@@ -30,6 +30,6 @@ public class DemoTest {
         }
         logger.info("logger sp finished");
 
-        logger.info("DemoTest finished.");
+        logger.info("main finished.");
     }
 }
--- demo-01-log4j2/src/test/java/com/suyh/DemoTest.java
+++ demo-01-log4j2/src/test/java/com/suyh/DemoTest.java
@@ -1,19 +1,22 @@
+package com.suyh;
 
+import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Main {
+public class DemoTest {
     static {
         System.setProperty("log4j.configurationFile", "conf/log4j2.xml");
         // 对应log4j2.xml 配置文件中使用的 ${sys:log4j2.dir:-demo-log}
-        System.setProperty("log4j2.dir", "demo-01");
+        System.setProperty("log4j2.dir", "demo-test");
     }
 
-    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+    private static final Logger logger = LoggerFactory.getLogger(DemoTest.class);
     private static final Logger logSp = LoggerFactory.getLogger("spLogger");
 
-    public static void main(String[] args) {
-        logger.info("main starting...");
+    @Test
+    public void t1() {
+        logger.info("DemoTest starting...");
         for (long i = 1; i < 10; i++) {
             logger.info("i: " + i);
             try {
@@ -29,6 +32,6 @@ public class Main {
         }
         logger.info("logger sp finished");
 
-        logger.info("main finished.");
+        logger.info("DemoTest finished.");
     }
 }
