c223caaeec31acb7a763a23ad554e0de0aaa454d
suyh <su_787910081@sina.com>
2019-12-26 17:19:07 +0800

    pom maven modules

--- demo-04-springboot-test/src/main/java/com/suyh/App.java
+++ demo-04-springboot-test/src/main/java/com/suyh/App.java
@@ -1,11 +1,24 @@
 package com.suyh;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class App {
+    static {
+        System.setProperty("log4j2.dir", "demo-04-springboot-test");
+    }
+
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
     public static void main(String[] args) {
         SpringApplication.run(App.class);
+        logger.info("begin...");
+
+        logger.debug("debug log.");
+        logger.info("info log.");
+        logger.warn("warn log.");
     }
 }
