c4562f22f00394bcdd0a10e47cf3644d0fa02203
suyh <su_787910081@sina.com>
2020-01-17 14:51:21 +0800

    springboot-properties 多配置文件

---
+++ demo-04-springboot-01-properties-01/src/main/java/com/suyh/SpringBootPropertiesMain.java
@@ -0,0 +1,12 @@
+package com.suyh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPropertiesMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPropertiesMain.class, args);
+    }
+}
