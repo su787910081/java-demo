3b091464c24c79e33d781d1017cf39438b072727
suyh <su_787910081@sina.com>
2020-01-23 22:19:29 +0800

    nothing

--- demo-05-mengxuetang-01/springcloud-parent-02-ha/meng-springcloud-ha-05-eureka-consumer-feign/src/main/java/com/suyh/controller/ProductControllerConsumer.java
+++ demo-05-mengxuetang-01/springcloud-parent-02-ha/meng-springcloud-ha-05-eureka-consumer-feign/src/main/java/com/suyh/controller/ProductControllerConsumer.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -19,22 +18,27 @@ public class ProductControllerConsumer {
     // private static final String REST_URL_PREFIX = "http://localhost:8001";
     // private static final String REST_URL_PREFIX = "http://microservice-product";
 
+    ProductClientService service;
+
+    // 构造注入
     @Autowired
-    ProductClientService productClient;
+    ProductControllerConsumer(ProductClientService service) {
+        this.service = service;
+    }
 
     @RequestMapping(value = "/consumer/product/add")
     public boolean add(Product product) {
-        return productClient.add(product);
+        return service.add(product);
     }
 
     @RequestMapping(value = "/consumer/product/get/{id}")
     public Product get(@PathVariable("id") Long id) {
-        return productClient.get(id);
+        return service.get(id);
     }
 
     @RequestMapping(value = "/consumer/product/list")
     public List<Product> list() {
-        return productClient.list();
+        return service.list();
     }
 
 
