93537fe3ae7845741fada5886f7e7ae33ac2f7af
su_787910081 <5uyunfe!>
2020-03-17 17:35:06 +0800

    @FeignClient(path = "/product") 统一前缀访问路径。

--- demo-05-mengxuetang-01/springcloud-parent-02-ha/meng-springcloud-ha-07-consumer-hystrix/src/main/java/com/suyh/service/ProductClientService.java
+++ demo-05-mengxuetang-01/springcloud-parent-02-ha/meng-springcloud-ha-07-consumer-hystrix/src/main/java/com/suyh/service/ProductClientService.java
@@ -10,16 +10,27 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import java.util.List;
 
 // 指定调用微服务名称
-@FeignClient(value = "microservice-product", fallback = ProductClientServiceFallback.class)
+@FeignClient(value = "microservice-product",
+        // 所有方法级映射要使用的前缀。
+        // 我的理解就是下面所有的方法的 @RequestMapping 都自动添加一个  "/product" 的访问前缀
+        // 相当于在类上加 requestMapping
+        path = "/product",
+        fallback = ProductClientServiceFallback.class)
 public interface ProductClientService {
 
-    @RequestMapping(value = "/product/add", method = RequestMethod.POST)
+    // 因为@FeignClient 注解中添加了 path="/product" 所以这里的访问路径是: /product/add
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    // @RequestMapping(value = "/product/add", method = RequestMethod.POST)
     boolean add(@RequestBody Product product);
 
-    @RequestMapping(value = "/product/get/{id}", method = RequestMethod.GET)
+    // 因为@FeignClient 注解中添加了 path="/product" 所以这里的访问路径是: /product/get/{id}
+    @RequestMapping(value = "/get/{id}", method = RequestMethod.GET)
+    // @RequestMapping(value = "/product/get/{id}", method = RequestMethod.GET)
     Product get(@PathVariable("id") Long id);
 
-    @RequestMapping(value = "/product/list", method = RequestMethod.GET)
+    // 因为@FeignClient 注解中添加了 path="/product" 所以这里的访问路径是: /product/list
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    // @RequestMapping(value = "/product/list", method = RequestMethod.GET)
     List<Product> list();
 
 }
