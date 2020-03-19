7900fb6a6359f584d22989d1459b15f014532d7e
su_787910081 <5uyunfe!>
2020-03-19 15:22:02 +0800

    mybatis generator 自动生成代码

--- demo-02-springboot-06-mybatis-generator-01/src/main/java/com/suyh/springboot/Application.java
+++ demo-02-springboot-06-mybatis-generator-01/src/main/java/com/suyh/springboot/Application.java
@@ -1,4 +1,4 @@
-package com.hello.springboot;
+package com.suyh.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
