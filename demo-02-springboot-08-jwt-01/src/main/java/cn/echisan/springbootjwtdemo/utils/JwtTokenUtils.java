6d496a29b5c95a73202380f912da104b61c75ad3
su_787910081 <5uyunfe!>
2020-03-23 17:49:04 +0800

    commit

--- demo-02-springboot-08-jwt-01/src/main/java/cn/echisan/springbootjwtdemo/utils/JwtTokenUtils.java
+++ demo-02-springboot-08-jwt-01/src/main/java/cn/echisan/springbootjwtdemo/utils/JwtTokenUtils.java
@@ -29,27 +29,35 @@ public class JwtTokenUtils {
     private static final long EXPIRATION_REMEMBER = 604800L;
 
     // 创建token
-    public static String createToken(String username,String role, boolean isRememberMe) {
+    public static String createToken(String username, String role, boolean isRememberMe) {
         long expiration = isRememberMe ? EXPIRATION_REMEMBER : EXPIRATION;
         HashMap<String, Object> map = new HashMap<>();
         map.put(ROLE_CLAIMS, role);
-        return Jwts.builder()
+        return Jwts.builder()//这里其实就是new一个JwtBuilder，设置jwt的body
+                // 设置签名使用的签名算法和签名使用的秘钥
                 .signWith(SignatureAlgorithm.HS512, SECRET)
+                // 如果有私有声明，一定要先设置这个自己创建的私有的声明，
+                // 这个是给builder的claim赋值，一旦写在标准的声明赋值之后，就是覆盖了那些标准的声明的
                 .setClaims(map)
+                //设置jti(JWT ID)：是JWT的唯一标识，根据业务需要，这个可以设置为一个不重复的值，
+                // 主要用来作为一次性token,从而回避重放攻击。
+                // .setId(id)
                 .setIssuer(ISS)
+                // sub(Subject)：代表这个JWT的主体，即它的所有人，这个是一个json格式的字符串，
+                // 可以存放什么userid，roldid之类的，作为什么用户的唯一标志。
                 .setSubject(username)
-                .setIssuedAt(new Date())
+                .setIssuedAt(new Date()) //iat: jwt的签发时间
                 .setExpiration(new Date(System.currentTimeMillis() + expiration * 1000))
                 .compact();
     }
 
     // 从token中获取用户名
-    public static String getUsername(String token){
+    public static String getUsername(String token) {
         return getTokenBody(token).getSubject();
     }
 
     // 获取用户角色
-    public static String getUserRole(String token){
+    public static String getUserRole(String token) {
         return (String) getTokenBody(token).get(ROLE_CLAIMS);
     }
 
@@ -62,7 +70,7 @@ public class JwtTokenUtils {
         }
     }
 
-    private static Claims getTokenBody(String token){
+    private static Claims getTokenBody(String token) {
         return Jwts.parser()
                 .setSigningKey(SECRET)
                 .parseClaimsJws(token)
