416cac923937f17ed377f78ec3173a2279e37e58
su_787910081 <5uyunfe!>
2020-03-24 16:50:10 +0800

    springboot shiro 框架demo

---
+++ demo-02-springboot-09-shiro-01/src/main/java/com/suyh/entity/User.java
@@ -0,0 +1,62 @@
+package com.suyh.entity;
+
+import java.util.Set;
+
+public class User {
+    private String id;
+    private String userName;
+    private String password;
+    /**
+     * 用户对应的角色集合
+     */
+    private Set<Role> roles;
+
+    public User(String id, String userName, String password, Set<Role> roleSet) {
+        this.id = id;
+        this.userName = userName;
+        this.password = password;
+        this.roles = roleSet;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                ", roles=" + roles +
+                '}';
+    }
+}
