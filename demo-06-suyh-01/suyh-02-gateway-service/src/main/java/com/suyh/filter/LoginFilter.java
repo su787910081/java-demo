cadff31806a48993c017fb7d476118dc01a16c55
suyh <su_787910081@sina.com>
2020-04-05 16:51:31 +0800

    commit

--- demo-06-suyh-01/suyh-02-gateway-service/src/main/java/com/suyh/filter/LoginFilter.java
+++ demo-06-suyh-01/suyh-02-gateway-service/src/main/java/com/suyh/filter/LoginFilter.java
@@ -3,7 +3,6 @@ package com.suyh.filter;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
-import org.apache.commons.lang.StringUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
