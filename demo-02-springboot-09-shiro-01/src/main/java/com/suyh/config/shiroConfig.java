ef1ca2c2dfbf75b5086b4682736030e413065069
suyh <su_787910081@sina.com>
2020-04-05 16:50:46 +0800

    commit

--- demo-02-springboot-09-shiro-01/src/main/java/com/suyh/config/shiroConfig.java
+++ demo-02-springboot-09-shiro-01/src/main/java/com/suyh/config/shiroConfig.java
@@ -6,15 +6,22 @@ import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSource
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.web.filter.DelegatingFilterProxy;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
 public class shiroConfig {
+    @Autowired
+    private CustomRealm customRealm;
+
     //不加这个注解不生效，具体不详
     @Bean
     @ConditionalOnMissingBean
@@ -25,22 +32,22 @@ public class shiroConfig {
     }
 
     //将自己的验证方式加入容器
-    @Bean
-    public CustomRealm myShiroRealm() {
-        CustomRealm customRealm = new CustomRealm();
-        return customRealm;
-    }
+//    @Bean
+//    public CustomRealm myShiroRealm() {
+//        CustomRealm customRealm = new CustomRealm();
+//        return customRealm;
+//    }
 
     //权限管理，配置主要是Realm的管理认证
     @Bean
     public SecurityManager securityManager() {
         DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
-        securityManager.setRealm(myShiroRealm());
+        securityManager.setRealm(customRealm);
         return securityManager;
     }
 
     //Filter工厂，设置对应的过滤条件和跳转条件
-    @Bean
+    @Bean("shiroFilter")
     public ShiroFilterFactoryBean shiroFilterFactoryBean(SecurityManager securityManager) {
         ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
         shiroFilterFactoryBean.setSecurityManager(securityManager);
@@ -57,6 +64,17 @@ public class shiroConfig {
         shiroFilterFactoryBean.setUnauthorizedUrl("/error");
         shiroFilterFactoryBean.setFilterChainDefinitionMap(map);
         return shiroFilterFactoryBean;
+    }
+
+    @Bean
+    @DependsOn(value = {"shiroFilter"})
+    public FilterRegistrationBean delegatingFilterProxy(){
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        DelegatingFilterProxy proxy = new DelegatingFilterProxy();
+        proxy.setTargetFilterLifecycle(true);
+        proxy.setTargetBeanName("shiroFilter");
+        filterRegistrationBean.setFilter(proxy);
+        return filterRegistrationBean;
     }
 
     //加入注解的使用，不加入这个注解不生效
