15cbae9b40a52b537b1cbe144fb600b2acafcb64
suyh <su_787910081@sina.com>
2020-04-06 15:24:20 +0800

    Zuul 网关的处理

--- demo-06-suyh-01/suyh-02-gateway-service/src/main/java/com/suyh/GatewayApplication.java
+++ demo-06-suyh-01/suyh-02-gateway-service/src/main/java/com/suyh/GatewayApplication.java
@@ -4,11 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableFeignClients
 // 网关代理注解，启用网关别忘了添加这个注解。否则将不起作用。
 @EnableZuulProxy
 public class GatewayApplication {
--- demo-06-suyh-01/suyh-04-sso-service/src/main/java/com/suyh/SsoServiceApplication.java
+++ demo-06-suyh-01/suyh-04-sso-service/src/main/java/com/suyh/SsoServiceApplication.java
@@ -3,8 +3,6 @@ package com.suyh;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 // 解除自动加载数据库连接配置
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
