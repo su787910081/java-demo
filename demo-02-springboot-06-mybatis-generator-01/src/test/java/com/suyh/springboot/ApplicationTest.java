81ad1fa40d6725a74afa7ad11669df470a0669d0
suyh <su_787910081@sina.com>
2020-04-12 20:30:38 +0800

    commit

--- demo-02-springboot-06-mybatis-generator-01/src/test/java/com/suyh/springboot/ApplicationTest.java
+++ demo-02-springboot-06-mybatis-generator-01/src/test/java/com/suyh/springboot/ApplicationTest.java
@@ -1,15 +1,11 @@
 package com.suyh.springboot;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+
+
 public class ApplicationTest {
 
-    @Test
+
     public void test01() {
     }
 }
