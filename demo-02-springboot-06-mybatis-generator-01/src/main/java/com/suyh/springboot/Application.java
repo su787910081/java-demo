427c24ca9d1b16da152bd9e0e3a443758a85c930
suyh <su_787910081@sina.com>
2020-04-12 20:30:11 +0800

    commit

--- demo-02-springboot-06-mybatis-generator-01/src/main/java/com/suyh/springboot/Application.java
+++ demo-02-springboot-06-mybatis-generator-01/src/main/java/com/suyh/springboot/Application.java
@@ -1,11 +1,4 @@
 package com.suyh.springboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class Application {
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
 }
