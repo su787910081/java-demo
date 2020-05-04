da1ab3eb99d1374cef38dd0c73337211c2a0f251
su_787910081 <5uyunfe!>
2020-05-04 09:30:35 +0800

    热部署，还没试过。

--- demo-02-springboot-11-init-01/src/main/java/com/suyh/init/ApplicationRunnerInit.java
+++ demo-02-springboot-11-init-01/src/main/java/com/suyh/init/ApplicationRunnerInit.java
@@ -1,6 +1,7 @@
 package com.suyh.init;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -23,8 +24,12 @@ public class ApplicationRunnerInit implements ApplicationRunner {
     @Autowired
     private ConfigurableApplicationContext ctx;
 
+    @Value("${server.port}")
+    private String port;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
         System.out.println("ApplicationRunnerInit ctx: " + ctx);
+        System.out.println("init, port: " + port);
     }
 }
