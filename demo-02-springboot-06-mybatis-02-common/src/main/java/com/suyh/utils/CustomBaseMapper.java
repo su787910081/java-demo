14b3f65c5910ae136ea66660fa46c5be2e51df6b
su_787910081 <5uyunfe!>
2020-05-07 18:47:04 +0800

    表的实体类字段，添加数据库类型

--- demo-02-springboot-06-mybatis-02-common/src/main/java/com/suyh/utils/CustomBaseMapper.java
+++ demo-02-springboot-06-mybatis-02-common/src/main/java/com/suyh/utils/CustomBaseMapper.java
@@ -3,6 +3,7 @@ package com.suyh.utils;
 import com.suyh.utils.impl.CustomBaseMapperProvider;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.SelectProvider;
+import org.apache.ibatis.annotations.UpdateProvider;
 import tk.mybatis.mapper.annotation.RegisterMapper;
 
 import java.util.List;
@@ -11,4 +12,23 @@ import java.util.List;
 public interface CustomBaseMapper<Model, Filter>  {
     @SelectProvider(type = CustomBaseMapperProvider.class, method = "dynamicSQL")
     List<Model> selectModelByFilter(@Param("filter") Filter filter);
+
+
+    /**
+     * 更新记录，指定过滤条件
+     * @param model
+     * @param filter
+     * @return
+     */
+    @UpdateProvider(type = CustomBaseMapperProvider.class, method = "dynamicSQL")
+    int updateModelByFilter(@Param("model") Model model, @Param("filter") Filter filter);
+
+    /**
+     * 过滤(模糊)查询
+     * @param filter
+     * @return
+     */
+    @SelectProvider(type = CustomBaseMapperProvider.class, method = "dynamicSQL")
+    List<Model> selectModelByFilterLike(@Param("filter") Filter filter);
+
 }
--- demo-02-springboot-06-mybatis-02-common/src/main/java/com/suyh/utils/impl/CustomBaseMapperProvider.java
+++ demo-02-springboot-06-mybatis-02-common/src/main/java/com/suyh/utils/impl/CustomBaseMapperProvider.java
@@ -14,17 +14,20 @@ import java.util.Set;
 
 public class CustomBaseMapperProvider extends MapperTemplate {
 
+
     public CustomBaseMapperProvider(Class<?> mapperClass, MapperHelper mapperHelper) {
         super(mapperClass, mapperHelper);
     }
 
     /**
-     * 过滤查询，这里的过滤参数必须要跟接口那里指定的参数一致。否则会错的。
+     * 过滤查询
      *
      * @param ms
      * @return
      */
     public SqlNode selectModelByFilter(MappedStatement ms) {
+        String paramFilter = "filter";
+
         Class<?> entityClass = getEntityClass(ms);
         setResultType(ms, entityClass);
         String tableName = this.tableName(entityClass);
@@ -35,16 +38,58 @@ public class CustomBaseMapperProvider extends MapperTemplate {
         List<SqlNode> sqlNodes = new ArrayList<>();
         sqlNodes.add(new StaticTextSqlNode(selectAll + fromTable));
 
-        String paramFilter = "filter";
+        StaticTextSqlNode columnNode = new StaticTextSqlNode("");
+        IfSqlNode ifFilterNode = new IfSqlNode(columnNode, paramFilter + " != null");
+        // ifFilterNode.
 
+        List<SqlNode> ifNodes = makeIfFilterNodes(entityClass, paramFilter);
+//        Set<EntityColumn> columns = EntityHelper.getColumns(entityClass);
+//        List<SqlNode> ifNodes = new ArrayList<>();
+//        for (EntityColumn column : columns) {
+//            // <if test="filter.createdBy != null and filter.createdBy != '' ">
+//            //   AND created_by = #{filter.createdBy, jdbcType=NVARCHAR}
+//            // </if>
+//
+//            // AND created_by = #{filter.createdBy, jdbcType=NVARCHAR}
+//            String sqlText = String.format("AND %s = #{%s.%s, jdbcType = %s}",
+//                    column.getColumn(), paramFilter, column.getProperty(), column.getJdbcType().toString());
+//
+//            StaticTextSqlNode columnNode = new StaticTextSqlNode(sqlText);
+//            if (column.getJavaType().equals(String.class)) {
+//                // filter.createdBy != null and filter.createdBy != ''
+//                String sqlTextString = String.format("%s.%s != null and %s.%s != ''",
+//                        paramFilter, column.getProperty(), paramFilter, column.getProperty());
+//                ifNodes.add(new IfSqlNode(columnNode, sqlTextString));
+//            } else {
+//                // filter.createdBy != null
+//                String sqlTextOther = String.format("%s.%s != null",
+//                        paramFilter, column.getProperty());
+//                ifNodes.add(new IfSqlNode(columnNode, sqlTextOther));
+//            }
+//        }
 
+        WhereSqlNode whereSqlNode = new WhereSqlNode(ms.getConfiguration(), new MixedSqlNode(ifNodes));
+
+        sqlNodes.add(whereSqlNode);
+        return new MixedSqlNode(sqlNodes);
+    }
+
+    /**
+     * 生成过滤条件的 SqlNode
+     *      <![CDATA[
+     *             <if test="filter.createdBy != null and filter.createdBy != '' ">
+     *               AND created_by = #{filter.createdBy, jdbcType=NVARCHAR}
+     *             </if>
+     *       ]]>
+     *
+     * @param entityClass
+     * @param paramFilter
+     * @return
+     */
+    private List<SqlNode> makeIfFilterNodes(Class<?> entityClass, String paramFilter) {
         Set<EntityColumn> columns = EntityHelper.getColumns(entityClass);
         List<SqlNode> ifNodes = new ArrayList<>();
         for (EntityColumn column : columns) {
-            // <if test="filter.createdBy != null and filter.createdBy != '' ">
-            //   AND created_by = #{filter.createdBy, jdbcType=NVARCHAR}
-            // </if>
-
             // AND created_by = #{filter.createdBy, jdbcType=NVARCHAR}
             String sqlText = String.format("AND %s = #{%s.%s, jdbcType = %s}",
                     column.getColumn(), paramFilter, column.getProperty(), column.getJdbcType().toString());
@@ -63,11 +108,75 @@ public class CustomBaseMapperProvider extends MapperTemplate {
             }
         }
 
-        WhereSqlNode whereSqlNode = new WhereSqlNode(ms.getConfiguration(), new MixedSqlNode(ifNodes));
+        return ifNodes;
+    }
 
+    /**
+     * 生成更新过滤条件的 SqlNode
+     *      <![CDATA[
+     *             <if test="null != model.uuid">
+     *               UUID = #{model.uuid, jdbcType = NVARCHAR},
+     *             </if>
+     *       ]]>
+     *
+     * @param entityClass
+     * @param paramModel
+     * @return
+     */
+    private List<SqlNode> makeIfModelNodes(Class<?> entityClass, String paramModel) {
+        // TODO:
+        return null;
+    }
+
+
+    /**
+     * 更新记录，指定过滤条件
+     *
+     * @return
+     */
+    public SqlNode updateModelByFilter(MappedStatement ms) {
+        String paramModel = "model";
+        String paramFilter = "filter";
+
+        Class<?> entityClass = getEntityClass(ms);
+        setResultType(ms, entityClass);
+        String tableName = this.tableName(entityClass);
+
+        List<SqlNode> sqlNodes = new ArrayList<>();
+        sqlNodes.add(new StaticTextSqlNode(SqlHelper.updateTable(entityClass, tableName)));
+
+        // 要更新的字段
+        List<SqlNode> sqlSetIfModelNodes = new ArrayList<>();
+        // 过滤条件字段
+        List<SqlNode> sqlWhereIfFilterNodes = new ArrayList<>();
+
+
+
+        sqlSetIfModelNodes = makeIfModelNodes(entityClass, paramModel);
+        sqlWhereIfFilterNodes = makeIfFilterNodes(entityClass, paramFilter);
+        // <if test="null != model">
+//        String ifModelText = paramModel + " != null";
+//        IfSqlNode sqlIfModelNode = new IfSqlNode(new StaticTextSqlNode(""), ifModelText);
+
+
+        SetSqlNode setSqlNode = new SetSqlNode(ms.getConfiguration(), new MixedSqlNode(sqlSetIfModelNodes));
+        WhereSqlNode whereSqlNode = new WhereSqlNode(ms.getConfiguration(), new MixedSqlNode(sqlWhereIfFilterNodes));
+        sqlNodes.add(setSqlNode);
         sqlNodes.add(whereSqlNode);
         return new MixedSqlNode(sqlNodes);
     }
 
+
+    /**
+     * 过滤(模糊)查询
+     *
+     * @return
+     */
+    public SqlNode selectModelByFilterLike(MappedStatement ms) {
+        String paramFilter = "filter";
+
+        // TODO:
+        return null;
+    }
 
 }
