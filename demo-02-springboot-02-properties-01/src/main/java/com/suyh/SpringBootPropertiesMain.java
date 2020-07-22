843a61eaef7651282d12ff68c6cec02a95437d4a
su787910081 <su_787910081@sina.com>
2020-07-22 10:13:41 +0800

    Update SpringBootPropertiesMain.java

--- demo-02-springboot-02-properties-01/src/main/java/com/suyh/SpringBootPropertiesMain.java
+++ demo-02-springboot-02-properties-01/src/main/java/com/suyh/SpringBootPropertiesMain.java
@@ -3,6 +3,8 @@ package com.suyh;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+chakan:  https://www.cnblogs.com/qiuting/p/9828515.html
+
 @SpringBootApplication
 public class SpringBootPropertiesMain {
 
