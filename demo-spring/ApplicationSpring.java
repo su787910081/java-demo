bd6931cf3b9aa62bd57f87739f5eea12b7de0f84
su787910081 <su_787910081@sina.com>
2021-01-07 15:04:36 +0800

    Create ApplicationSpring.java

---
+++ demo-spring/ApplicationSpring.java
@@ -0,0 +1,21 @@
+package com.suyh;
+
+import com.suyh.service.HelloService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author 苏雲弘
+ * @since 2021-01-07
+ */
+public class ApplicationSpring {
+    public static void main(String[] args) {
+        ApplicationContext ac = new ClassPathXmlApplicationContext("classpath*:META-INF/spring/*.xml");
+        System.out.println("Hello World!");
+        HelloService helloService = (HelloService) ac.getBean("helloService");
+        System.out.println(helloService.say());
+
+        helloService = ac.getBean(HelloService.class);
+        System.out.println(helloService.say());
+    }
+}
