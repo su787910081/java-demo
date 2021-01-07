86d7527b09e50b600e7fea0a9a14d1a9ddaa4799
苏云弘 <su_787910081@sina.com>
2021-01-07 22:27:33 +0800

    commit

--- demo-spring/src/main/java/com/suyh/spring/ApplicationSpring.java
+++ demo-spring/src/main/java/com/suyh/spring/ApplicationSpring.java
@@ -1,6 +1,7 @@
 package com.suyh.spring;
 
 import com.suyh.spring.service.HelloService;
+import com.suyh.spring.service.MessageService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -11,11 +12,13 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class ApplicationSpring {
     public static void main(String[] args) {
         ApplicationContext ac = new ClassPathXmlApplicationContext("classpath*:META-INF/spring/*.xml");
-        System.out.println("Hello World!");
         HelloService helloService = (HelloService) ac.getBean("helloService");
         System.out.println(helloService.say());
 
         helloService = ac.getBean(HelloService.class);
         System.out.println(helloService.say());
+
+        MessageService messageService = ac.getBean(MessageService.class);
+        System.out.println(messageService.getMessage());
     }
 }
