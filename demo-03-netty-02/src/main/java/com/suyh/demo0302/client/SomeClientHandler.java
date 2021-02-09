fa76c658bcb980eb30fe86b97f23185a73947640
suyh <su_787910081@sina.com>
2021-02-09 17:43:18 +0800

    netty

--- demo-03-netty-02/src/main/java/com/suyh/demo0302/client/SomeClientHandler.java
+++ demo-03-netty-02/src/main/java/com/suyh/demo0302/client/SomeClientHandler.java
@@ -9,6 +9,12 @@ import java.util.concurrent.TimeUnit;
 public class SomeClientHandler extends SimpleChannelInboundHandler<String> {
 
     // msg的消息类型与类中的泛型类型是一致的
+
+    /**
+     * SimpleChannelInboundHandler 中的channelRead0 的msg 对象会被父类处理，并释放掉。
+     * 所以在这里的实现中如果异步写用到了msg 会出现使用了被标记为释放的数据，所以如果要使用到msg
+     * 做异步操作，则可以选择ChannelInboundHandlerAdapter
+     */
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
         System.out.println(ctx.channel().remoteAddress() + "，" + msg);
@@ -16,7 +22,7 @@ public class SomeClientHandler extends SimpleChannelInboundHandler<String> {
         TimeUnit.MILLISECONDS.sleep(500);
     }
 
-    // 当Channel被激活后会触发该方法的执行
+    // 当Channel被激活后会触发该方法的执行，即连接成功。
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
         ctx.channel().writeAndFlush("from client：begin talking");
--- demo-03-netty-02/src/main/java/com/suyh/demo0302/server/SomeServerHandler.java
+++ demo-03-netty-02/src/main/java/com/suyh/demo0302/server/SomeServerHandler.java
@@ -8,6 +8,10 @@ import java.util.concurrent.TimeUnit;
 
 public class SomeServerHandler extends ChannelInboundHandlerAdapter {
 
+    /**
+     * ChannelInboundHandlerAdapter 中的channelRead 方法不会为我们释放msg 的数据，所以正常情况我们需要对该msg 进行释放
+     * 如果有异步的写操作，用到了msg，则不可释放。需要在异步中使用结束 了再进行释放操作。
+     */
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
 
